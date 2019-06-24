7cc198d1964c10fa8c034b7eaa85bb3e049ac3a7
Danilo Vitoriano <vitoriano08@gmail.com>
2019-06-24 00:59:04 -0300

    add mongoose schema and post route

--- app.js
+++ app.js
@@ -1,7 +1,44 @@
 const express = require('express')
+const mongoose = require('mongoose');
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.json());
+
+// mongoose
+mongoose.connect('mongodb://127.0.0.1:27017/blog', { useNewUrlParser: true });
+var db = mongoose.connection;
+var blogSchema = new mongoose.Schema({
+    name: String
+});
+var Blog = mongoose.model('Blog', blogSchema);
+
+// db connection
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log("we're connected!");
+});
+
+// routes
+app.get('/', (req, res) => {
+    Blog.find(function (err, posts) {
+        if (err) return console.error(err);
+        res.json(posts)
+    })
+})
+
+app.post('/new', (req, res) => {
+    var name = req.body.name
+    new Blog({ name }).save();
+    console.log("item added: ", name)
+    res.status(201)
+    res.send(req.body.name)
+})
 
 app.listen(port, () => console.log(`Example app listening on http://localhost:${port}`))
+
+
+
+
+
